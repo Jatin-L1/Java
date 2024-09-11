a1e860811a1d7a51b979ba52d971c4e5fe2783c4
Jatin-L1 <jatinsharmasm2435@gmail.com>
2024-09-11 23:46:14 +0530

    learnt merge sort again and the concept of list inside a list

--- Merge_Sort/practice.java
+++ Merge_Sort/practice.java
@@ -1,57 +1,94 @@
 package Merge_Sort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
-import Assignments.Arrays.mergetwo;
-import Assignments.Strings.ismorphic;
-
 public class practice {
-
-
-
+    // Define answer as an ArrayList of Integer
+    static ArrayList<ArrayList<Integer>> answer = new ArrayList<>();
 
     public static void main(String[] args) {
-        int [] arr = {2,4,6,8};
-        int [] brr = {1,3,5,7,9};
-        int [] ans = new int[arr.length+brr.length];
-        System.out.println(Arrays.toString(mergetwosortedarray(arr,brr , ans)));
-
+        int[] array = {80, 30, 50, 20, 60, 10, 70, 40};
+        answer = new ArrayList<>();
+        mergeSort(array); // Perform merge sort
+        System.out.println(answer); // Print the answer
     }
 
-    private static int[] mergetwosortedarray(int[] arr, int[] brr, int[] ans) {
-        int i =0;
-        int j =0;
+    private static void merge(int[] arr, int[] brr, int[] ans) {
+        int i = 0;
+        int j = 0;
         int c = 0;
 
-        while (i<arr.length && j<brr.length) {
+        while (i < arr.length && j < brr.length) {
 
-            if(arr[i]>brr[j]){
+            if (arr[i] > brr[j]) {
                 ans[c] = brr[j];
                 c++;
                 j++;
-            }
-            if(arr[i]<brr[j]){
-                ans[c] = arr[i];
-                c++;
-                i++;
-            }
-            
-        }
-        if(i==arr.length){
-            while(j<brr.length) {
-                ans[c] = brr[j];
-                c++;
-                j++;
-            }
-        }
-        if(j==brr.length){
-           while (i<arr.length) {
-                ans[c] = arr[i];
+            } else {
+                ans[c] = arr[i]; 
                 c++;
                 i++;
             }
         }
-        return ans ;
-    } 
-    
+
+        if (i == arr.length) {
+            while (j < brr.length) {
+                ans[c] = brr[j];
+                c++;
+                j++;
+            }
+        }
+        if (j == brr.length) {
+            while (i < arr.length) {
+                ans[c] = arr[i];
+                c++;
+                i++;
+            }
+        }
+    }
+
+    public static ArrayList<ArrayList<Integer>> mergeSort(int[] arr) {
+        int n = arr.length;
+        // if (n > 1) {
+        //     // Convert int[] to ArrayList<Integer> and add to the answer
+        //     ArrayList<Integer> currentList = new ArrayList<>();
+        //     for (int num : arr) {
+        //         currentList.add(num); // Add each element to the current list
+        //     }
+        //     answer.add(currentList); // Add the current sorted array to answer
+        // } else {
+        //     return answer;
+        // }
+        if (n==1) {
+            return answer;
+        }
+
+        // Splitting the array into two halves
+        int[] a = new int[n / 2];
+        int[] b = new int[n - n / 2];
+
+        for (int i = 0; i < n / 2; i++) {
+            a[i] = arr[i];
+        }
+        for (int i = 0; i < n - n / 2; i++) {
+            b[i] = arr[i + n / 2];
+        }
+
+        // Recursively call mergeSort on both halves
+        mergeSort(a);
+        mergeSort(b);
+
+        // Merging both halves
+        merge(a, b, arr);
+
+      
+        ArrayList<Integer> mergedList = new ArrayList<>();
+        for (int num : arr) {
+            mergedList.add(num); 
+        }
+        answer.add(mergedList);
+
+        return answer;
+    }
 }
